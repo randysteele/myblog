85ed096c6b30f26affce3343b67a52b6597df6d9
Randy Steele <randysteele@Randys-iMac.lan>
2023-07-22 00:20:58 -0400

    corrected error with comments not creating properly. Updated user.username to user.email in the comments index

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+  before_action :set_comment, only: [:show, :edit, :update]
+
     def new 
         if params[:article_id] && @article = Article.find_by_id(params[:article_id])
           @comment = @article.comments.build
@@ -8,25 +10,41 @@ class CommentsController < ApplicationController
         end
       end
 
+    # def create
+    #     @comment = @article.comments.create(params[:comment].permit(:name, :comment, :article_id, :user_id))
+    #     redirect_to article_path(@article)    
+    # end
+
     def create
-        @article = Article.find(params[:article_id])
-          @comment = @article.comments.create(params[:comment].permit(:name, :comment))
-        redirect_to article_path(@article)    
+      @comment = current_user.comments.build(comment_params)
+      if @comment.save
+        redirect_to article_comments_path
+      else
+        render :new
+      end
     end
 
     def index
+      if params[:article_id] && @article = Article.find_by_id(params[:article_id])
+        @comments = @article.comments
+      else
         @comments = Comment.all
+        end
       end
     
   
     def destroy
-        @post = Article.find(params[:article_id])
+        @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
         @comment.destroy
         redirect_to article_path(@article)
     end
 
     def comment_params
-      params.require(:comment).permit(:name, :comment, :article_id)
+      params.require(:comment).permit(:name, :comment, :article_id, :user_id)
     end
+
+    def set_comment
+      @comment = Comment.find_by(id: params[:id])
+     end
 end
