aaa85453ef3efc500d53b269132d0bba9d971a4d
Randy Steele <randysteele@Randys-iMac.lan>
2023-07-24 00:18:31 -0400

    comments/replies

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,50 +1,35 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[ show edit update destroy ]
 
-  before_action :set_comment, only: [:show, :edit, :update]
+  def new 
+    @article = Article.fine(params[:article_id])
+    @comment = @article.comments.new(parent_id: params[:parent_id])
 
-    def new 
-        if params[:article_id] && @article = Article.find_by_id(params[:article_id])
-          @comment = @article.comments.new(parent_id: params[:parent_id])
-        else
-          @comment = Comment.new
-        end
-      end
+  end
 
-    # def create
-    #     @comment = @article.comments.create(params[:comment].permit(:name, :comment, :article_id, :user_id))
-    #     redirect_to article_path(@article)    
-    # end
+  def create
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.new(comment_params)
+      redirect_to article_path(@article)    
+  end
 
-    def create
-      @comment = current_user.comments.build(comment_params)
-      if @comment.save
-        redirect_to article_comments_path
-      else
-        render :new
-      end
+  def index
+      @comments = Comment.all
     end
-
-    def index
-      if params[:article_id] && @article = Article.find_by_id(params[:article_id])
-        @comments = @article.comments
-      else
-        @comments = Comment.all
-        end
-      end
-    
   
-    def destroy
-        @article = Article.find(params[:article_id])
-        @comment = @article.comments.find(params[:id])
-        @comment.destroy
-        redirect_to article_path(@article)
-    end
 
-    def comment_params
-      params.require(:comment).permit(:name, :comment, :article_id, :user_id)
-    end
+  def destroy
+      @post = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+      redirect_to article_path(@article)
+  end
 
-    def set_comment
-      @comment = Comment.find_by(id: params[:id])
-     end
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:name, :comment, :article_id, :parent_id, :user_id)
+  end
 end
